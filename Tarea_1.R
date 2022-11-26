792d6ac713f41d49f5b85f3100f9f2d5041d4153
Felipe Neira <69496089+felipeneira@users.noreply.github.com>
2022-11-26 20:10:33 -0300

    Update Tarea_1.R

--- Tarea_1.R
+++ Tarea_1.R
@@ -1,12 +1,18 @@
 library("readxl")
+library("dplyr")
+library("lmtest")
 # X e Y corresponden a la variable dependiente e independiente
 # W corresponde a el error estimado
 #
-datos <- read_excel("/home/felipe/Descargas/Datos #1.xlsx")
-summary(datos)
+datos <- read_excel("C:/Users/fneir/Downloads/Datos #1.xlsx")
 X <- as.matrix(datos %>% transmute (c = 1, r_us, r_ch, cobre, petroleo, broad_dollar))
 Y <- as.matrix(datos %>% transmute (tdc))
-B = solve(t(X)%*%X)%*%(t(X)%*%Y)
-Y_ = B[0]
+B <- solve(t(X)%*%X)%*%(t(X)%*%Y)
+e <- as.matrix(Y-B[1]-B[2]*X[,2]-B[3]*X[,3]-B[4]*X[,4]-B[5]*X[,5]-B[6]*X[,6])
+t <- length(datos$r_us)
+sigma2 = (t(e)%*%e)/(t-5-1)
+MV <- (-t/2*log(2*3.14*sigma2)-(1/2*sigma2)*sum(e^2))*(-1)
 
-B[1]*X[,2]
+prueba <- lm(data = datos, tdc ~ r_us+ r_ch + cobre + petroleo + broad_dollar)
+bptest(prueba)
+
